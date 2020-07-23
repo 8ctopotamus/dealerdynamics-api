03ffcd8e049142ccd83871b76c9c8ffafd2a78f8
8ctopotamus <joshnaylor88@gmail.com>
2020-07-22 21:38:56 -0500

    receive post json and insert into mysql db

---
+++ api/vechicles.php
@@ -0,0 +1,60 @@
+<?php
+
+// localhost
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'dealer_dynamics';
+
+// production
+// $dbHost = 'localhost';
+// $dbUsername = 'snowaywpfmt';
+// $dbPassword = 'fd7lxPWWuZheTl9IbPeJ';
+// $dbName = 'wp_snowaywpfmt';
+
+$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+if ($conn->connect_error) {
+  http_response_code (500);
+  die("Unable to connect database: " . $conn->connect_error);
+} 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $inputJSON = file_get_contents('php://input');
+  $vehicle = json_decode($inputJSON, TRUE); //convert JSON into array
+  $uuid = $vehicle['uuid'];
+
+  if (empty($uuid)) {  
+    http_response_code (500);
+    die('You must provide a uuid.');
+  }
+
+  $insertQuery = 'INSERT INTO vehicles (
+    uuid,
+    vin,
+    mileage,
+    year,
+    make,
+    model,
+    series,
+    style
+  ) VALUES (
+    "' . $uuid . '",
+    "' . $vehicle['vin'] . '",
+    ' . $vehicle['mileage'] . ',
+    ' . $vehicle['year'] . ',
+    "' . $vehicle['make'] . '",
+    "' . $vehicle['model'] . '",
+    "' . $vehicle['series'] . '",
+    "' . $vehicle['style'] . '"
+  )';
+
+  $result = $conn->query($insertQuery);
+  
+  if ($result) {
+    echo json_encode( [ 'partner_vehicle' => [ 'id' => $conn->insert_id] ] );
+    http_response_code (200);
+  } else {
+    http_response_code (500);
+    die();
+  }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d "{ \"uuid\": \"1kjsdf83o3j8r3\", \"vin\": \"32874oij398r38\", \"mileage\": 200000 , \"year\": 2914,  \"make\": \"Ford\", \"model\": \"Mustang\", \"series\": \"G3\", \"style\": \"red\" }" http://localhost/dealerdynamics-api/api/vechicles.php
